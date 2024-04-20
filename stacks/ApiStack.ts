e018483cce5d98f7038674d3b8d86f2eae1c1870
Patrick K. Etienne <git@paradox.limited>
2024-04-20 11:01:53 -0400

    Add an API to Handle Billing.

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -19,6 +19,7 @@ export function ApiStack({ stack }: StackContext) {
 			'GET /notes': 'packages/functions/src/list.main',
 			'PUT /notes/{id}': 'packages/functions/src/update.main',
 			'DELETE /notes/{id}': 'packages/functions/src/delete.main',
+			'POST /billing': 'packages/functions/src/billing.main',
 		},
 	});
 
