5f6062dfa572aadafcd0e82db760a3d967297406
Patrick K. Etienne <git@paradox.limited>
2024-04-20 17:07:50 -0400

    Add App Favicons.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -10,10 +8,10 @@ function App() {
 		<>
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+					viteLogo
 				</a>
 				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
+					reactLogo
 				</a>
 			</div>
 			<h1>Vite + React</h1>
