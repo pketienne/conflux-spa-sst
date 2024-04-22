bc7da18a000a95c4d3260b511682980dc57b6616
Patrick K. Etienne <git@paradox.limited>
2024-04-21 23:46:29 -0400

    Add the Create Note Page.

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from './containers/Home.tsx';
 import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
+import NewNote from './containers/NewNote.tsx';
 
 export default function Links() {
 	return (
@@ -11,6 +12,7 @@ export default function Links() {
 			<Route path="*" element={<NotFound />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<Signup />} />
+			<Route path="/notes/new" element={<NewNote />} />
 		</Routes>
 	);
 }
