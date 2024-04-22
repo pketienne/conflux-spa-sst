310593574aaaa3114bb4c31aba7ba0ec6437d7e1
Patrick K. Etienne <git@paradox.limited>
2024-04-21 22:22:17 -0400

    Create a Custom React Hook to Handle Form Fields.

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -7,16 +7,16 @@ import { useAppContext } from '../lib/contextLib';
 import { useNavigate } from 'react-router-dom';
 import LoaderButton from '../components/LoaderButton.tsx';
 import { onError } from '../lib/errorLib';
+import { useFormFields } from '../lib/hooksLib';
 
 export default function Login() {
 	const nav = useNavigate();
-	const [isLoading, setIsLoading] = useState(false);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
 	const { userHasAuthenticated } = useAppContext();
+	const [isLoading, setIsLoading] = useState(false);
+	const [fields, handleFieldChange] = useFormFields({ email: '', password: '' });
 
 	function validateForm() {
-		return email.length > 0 && password.length > 0;
+		return fields.email.length > 0 && fields.password.length > 0;
 	}
 
 	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -25,7 +25,7 @@ export default function Login() {
 		setIsLoading(true);
 
 		try {
-			await Auth.signIn(email, password);
+			await Auth.signIn(fields.email, fields.password);
 			userHasAuthenticated(true);
 			nav('/');
 		} catch (error) {
@@ -40,11 +40,11 @@ export default function Login() {
 				<Stack gap={3}>
 					<Form.Group controlId="email">
 						<Form.Label>Email</Form.Label>
-						<Form.Control autoFocus size="lg" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+						<Form.Control autoFocus size="lg" type="email" value={fields.email} onChange={handleFieldChange} />
 					</Form.Group>
 					<Form.Group controlId="password">
 						<Form.Label>Password</Form.Label>
-						<Form.Control size="lg" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+						<Form.Control size="lg" type="password" value={fields.password} onChange={handleFieldChange} />
 					</Form.Group>
 					<LoaderButton size="lg" type="submit" isLoading={isLoading} disabled={!validateForm()}>
 						Login
