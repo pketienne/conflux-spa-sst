2ce1e5f26b7e9481d38959453855a1d7bfc02de7
Patrick K. Etienne <git@paradox.limited>
2024-04-22 22:24:03 -0400

    Display a Note.

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -4,6 +4,7 @@ import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
 import NewNote from './containers/NewNote.tsx';
+import Notes from './containers/Notes.tsx';
 
 export default function Links() {
 	return (
@@ -13,6 +14,7 @@ export default function Links() {
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<Signup />} />
 			<Route path="/notes/new" element={<NewNote />} />
+			<Route path="/notes/:id" element={<Notes />} />
 		</Routes>
 	);
 }
