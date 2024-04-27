720053c92e8021841591c8c218b1933697733459
Patrick K. Etienne <git@paradox.limited>
2024-04-26 23:15:34 -0400

    Use the Redirect Routes.

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -6,17 +6,55 @@ import Signup from './containers/Signup.tsx';
 import NewNote from './containers/NewNote.tsx';
 import Notes from './containers/Notes.tsx';
 import Settings from './containers/Settings.tsx';
+import AuthenticatedRoute from './components/AuthenticatedRoute.tsx';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute.tsx';
 
 export default function Links() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route
+				path="/login"
+				element={
+					<UnauthenticatedRoute>
+						<Login />
+					</UnauthenticatedRoute>
+				}
+			/>
+			<Route
+				path="/signup"
+				element={
+					<UnauthenticatedRoute>
+						<Signup />
+					</UnauthenticatedRoute>
+				}
+			/>
+			<Route
+				path="/settings"
+				element={
+					<AuthenticatedRoute>
+						<Settings />
+					</AuthenticatedRoute>
+				}
+			/>
+			<Route
+				path="/notes/new"
+				element={
+					<AuthenticatedRoute>
+						<NewNote />
+					</AuthenticatedRoute>
+				}
+			/>
+
+			<Route
+				path="/notes/:id"
+				element={
+					<AuthenticatedRoute>
+						<Notes />
+					</AuthenticatedRoute>
+				}
+			/>
 			<Route path="*" element={<NotFound />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/signup" element={<Signup />} />
-			<Route path="/settings" element={<Settings />} />
-			<Route path="/notes/new" element={<NewNote />} />
-			<Route path="/notes/:id" element={<Notes />} />
 		</Routes>
 	);
 }
