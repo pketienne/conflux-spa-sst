127463b01e0cf9487c7ea90687982614f168a3fc
Patrick K. Etienne <git@paradox.limited>
2024-04-26 23:19:55 -0400

    Redirect on Login.

--- packages/frontend/src/containers/Login.tsx
+++ packages/frontend/src/containers/Login.tsx
@@ -4,13 +4,11 @@ import Stack from 'react-bootstrap/Stack';
 import './Login.css';
 import { Auth } from 'aws-amplify';
 import { useAppContext } from '../lib/contextLib';
-import { useNavigate } from 'react-router-dom';
 import LoaderButton from '../components/LoaderButton.tsx';
 import { onError } from '../lib/errorLib';
 import { useFormFields } from '../lib/hooksLib';
 
 export default function Login() {
-	const nav = useNavigate();
 	const { userHasAuthenticated } = useAppContext();
 	const [isLoading, setIsLoading] = useState(false);
 	const [fields, handleFieldChange] = useFormFields({ email: '', password: '' });
@@ -27,7 +25,6 @@ export default function Login() {
 		try {
 			await Auth.signIn(fields.email, fields.password);
 			userHasAuthenticated(true);
-			nav('/');
 		} catch (error) {
 			onError(error);
 			setIsLoading(false);
