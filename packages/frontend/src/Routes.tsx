9a1f5f069c87c994d903dc949662ca70957cb42d
Patrick K. Etienne <git@paradox.limited>
2024-04-26 20:40:30 -0400

    Create a Settings Page.

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -5,6 +5,7 @@ import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
 import NewNote from './containers/NewNote.tsx';
 import Notes from './containers/Notes.tsx';
+import Settings from './containers/Settings.tsx';
 
 export default function Links() {
 	return (
@@ -15,6 +16,7 @@ export default function Links() {
 			<Route path="/signup" element={<Signup />} />
 			<Route path="/notes/new" element={<NewNote />} />
 			<Route path="/notes/:id" element={<Notes />} />
+			<Route path="/settings" element={<Settings />} />
 		</Routes>
 	);
 }
